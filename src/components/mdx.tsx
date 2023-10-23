4c700b8129a53d02e875ce807554105714d00dec
Rashed Suwaidi <x.suwaidi@gmail.com>
2023-10-23 21:04:41 +0400

    color on oct

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -44,13 +44,23 @@ function ContentWrapper({
 }
 
 function ArticleHeader({ id, date }: { id: string; date: string | Date }) {
+  
+  // If the input date is string, convert it into Date. If it's already a date object, it will be as it is.
+  let dateObject = typeof date === 'string' ? new Date(date) : date;
+  
+  // Check if the date is October 29
+  const isSpecialDate = dateObject.getDate() === 29 && dateObject.getMonth() === 9;
+  
+  // Set the conditional class name based on whether it is a special date or not
+  const specialClassName = isSpecialDate ? 'text-orange-600 dark:text-orange-400' : '';
+
   return (
     <header className="relative mb-10 xl:mb-0">
       <div className="pointer-events-none absolute left-[max(-0.5rem,calc(50%-18.625rem))] top-0 z-50 flex h-4 items-center justify-end gap-x-2 lg:left-0 lg:right-[calc(max(2rem,50%-38rem)+40rem)] lg:min-w-[32rem] xl:h-8">
         <Link href={`#${id}`} className="inline-flex">
           <FormattedDate
-            date={date}
-            className="hidden xl:pointer-events-auto xl:block xl:text-2xs/4 xl:font-medium xl:text-white/50"
+            date={dateObject}
+            className={`hidden xl:pointer-events-auto xl:block xl:text-2xs/4 xl:font-medium xl:text-white/50 ${specialClassName}`}
           />
         </Link>
         <div className="h-[0.0625rem] w-3.5 bg-gray-400 lg:-mr-3.5 xl:mr-0 xl:bg-gray-300" />
@@ -59,8 +69,8 @@ function ArticleHeader({ id, date }: { id: string; date: string | Date }) {
         <div className="flex">
           <Link href={`#${id}`} className="inline-flex">
             <FormattedDate
-              date={date}
-              className="text-2xs/4 font-medium text-gray-500 dark:text-white/50 xl:hidden"
+              date={dateObject}
+              className={`text-2xs/4 font-medium text-gray-500 dark:text-white/50 xl:hidden ${specialClassName}`}
             />
           </Link>
         </div>
